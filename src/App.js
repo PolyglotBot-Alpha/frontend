b8497a1efae5ae79e2c5c88393b7d73295454b8c
Yuxuan Cai <1093369126@qq.com>
2024-05-01 22:07:43 -0400

    side bar update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from "./pages/Main";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main.js";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js";
+import GoogleSignIn from "./pages/GoogleSignIn.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter> already in index.js
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/GoogleSignIn" element={<GoogleSignIn />} />
+    </Routes>
+    // </BrowserRouter>
   );
 }
 
